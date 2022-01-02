e213999efdbdfa6fa2b5437372b8922a1f0a8898
Volga Mashchitskaya <Volgushka@list.ru>
2022-01-02 21:01:29 +0300

    Happy New Year!!

--- 13_FilesAndNetwork/homework_13.1/src/main/java/FileUtils.java
+++ 13_FilesAndNetwork/homework_13.1/src/main/java/FileUtils.java
@@ -1,6 +1,47 @@
+import java.io.File;
+import java.util.List;
+
 public class FileUtils {
 
     public static long calculateFolderSize(String path) {
-        return 0;
+
+        long count = 0;
+
+        File folder = new File (path);
+        File[] listOfFiles = folder.listFiles();
+
+        for (File file : listOfFiles) {
+            if (file.isFile())
+                count += file.length();
+            else
+                count += calculateFolderSize(file.getAbsolutePath());
+        }
+        return count;
+    }
+
+
+
+
+    public static String FolderSize(String path) {
+
+       double folderSize = calculateFolderSize(path);
+
+        if (folderSize < 1024) {
+            return folderSize + " б";
+
+        } else if (folderSize < 1048576) {
+            String result = String.format("%.1f",folderSize/1024);
+            return result + " Кб";
+
+        } else if (folderSize < 1073741824) {
+            String result = String.format("%.1f",folderSize/1048576);
+            return result  + " Мб";
+        }
+
+        String result = String.format("%.1f",folderSize/1073741824);
+        return result + " Гб";
     }
 }
+
+
+
--- 13_FilesAndNetwork/homework_13.1/src/main/java/Main.java
+++ 13_FilesAndNetwork/homework_13.1/src/main/java/Main.java
@@ -1,5 +1,39 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.InvalidPathException;
+import java.util.Scanner;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class Main {
+    private static final Logger logger = LogManager.getLogger(Main.class);
+
     public static void main(String[] args) {
 
+        System.out.println("Введите путь до папки: ");
+        Scanner scanner = new Scanner(System.in);
+
+        for (; ; ) {
+            String inputPath = scanner.nextLine();
+
+            try {
+
+                System.out.println("Размер папки :" + inputPath + " составляет: " + FileUtils.FolderSize(inputPath));
+                logger.info("Поиск размера папки: " + inputPath);
+
+            } catch (NullPointerException e) {
+
+                if (inputPath.split(" ").length > 1) {
+                    System.err.println(e.getMessage());
+                    System.err.println("Вы ввели пути двух папок.Необходимо ввести путь одной папки.");
+                }
+                else {
+                    System.err.println(e.getMessage());
+                    System.err.println("Неверный путь либо такой папки не существует");
+                }
+                logger.error("Неверный путь либо такой папки не существует: " + inputPath + " " + e);
+            }
+
+        }
     }
 }
