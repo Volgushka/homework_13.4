1c0f3e244f3c9eb009b97a8ed4fd289294c975f1
Volga Mashchitskaya <Volgushka@list.ru>
2022-01-05 16:50:58 +0300

    copy

--- 13_FilesAndNetwork/homework_13.2/src/main/java/FileUtils.java
+++ 13_FilesAndNetwork/homework_13.2/src/main/java/FileUtils.java
@@ -1,5 +1,49 @@
+import java.io.File;
+import java.nio.file.Files;
+
 public class FileUtils {
-    public static void copyFolder(String sourceDirectory, String destinationDirectory) {
-        // TODO: write code copy content of sourceDirectory to destinationDirectory
+
+    public static void copyFolder(String sourceDirectory, String destinationDirectory)
+    {
+
+        File srcFolder = new File(sourceDirectory);
+        File destFolder = new File(destinationDirectory);
+
+        if(srcFolder.isDirectory()){
+            try {
+
+                if (!destFolder.exists()) {
+                    destFolder.mkdir();
+                    System.out.println("Папка "
+                            + srcFolder + " создана  в " + destFolder);
+                }
+
+                String[] files = srcFolder.list();
+
+                for (String file : files) {
+                    File srcFile = new File(srcFolder, file);
+                    File destFile = new File(destFolder, file);
+
+                    copyFolder(srcFile.toString(), destFile.toString());
+                }
+            }
+            catch (Exception e){
+                e.printStackTrace();
+            }
+
+        }else{
+
+            try {
+            Files.copy(srcFolder.toPath(), destFolder.toPath());
+
+                System.out.println("Файл успешно скопирован.");
+
+            }
+            catch (Exception e){
+                e.printStackTrace();
+
+            }
+
+        }
     }
 }
--- 13_FilesAndNetwork/homework_13.2/src/main/java/Main.java
+++ 13_FilesAndNetwork/homework_13.2/src/main/java/Main.java
@@ -1,4 +1,33 @@
+import java.io.File;
+import java.util.Scanner;
+
 public class Main {
+
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        for (; ; ) {
+            System.out.println("Введите адрес копируемой папки: ");
+            String scr = scanner.nextLine();
+
+            System.out.println("Введите адрес папки назначения: ");
+            String dest = scanner.nextLine();
+
+            if (!new File(scr).exists()) {      // проверка наличия источника
+                System.out.println("Такой папки не существует");
+
+            } else {
+
+                try {
+                    FileUtils.copyFolder(scr, dest);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    System.exit(0);
+                }
+            }
+
+            System.out.println("Папка успешно скопирована");
+        }
     }
 }
